a2a2d817a6164d50e7193a2b10576a0e5a732b70
jinmo <yjmd2222@gmail.com>
2023-07-20 10:52:28 +0900

    id null인 것 빼고 db 적재 가능. 예약마감/예약 처리 필요

--- scripts/crawling/_test_rentcar.py
+++ scripts/crawling/_test_rentcar.py
@@ -1,9 +1,11 @@
 import time
+import sqlite3
 
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
 
 from bs4 import BeautifulSoup
 
@@ -24,15 +26,15 @@ def parse_date(date_: datetime, time_: str):
 
     # 앞에는 'Wednesday, August 9th, 2023'
     # 뒤에는 sql에 입력할 수 있는 timestamp
-    return datetime_.strftime(f"%A, %B {day}{suffix}, %Y"), datetime_.timestamp()
+    return datetime_, datetime_.strftime(f"%A, %B {day}{suffix}, %Y")
 
-def rentcar(s_date, e_date, s_time, e_time):
+def rentcar_crawl(s_date, e_date, s_time, e_time):
     '''
     s_date, e_date: datetime\n
     s_time, e_time: str('HH:MM')
     '''
-    s_date, s_timestamp = parse_date(s_date, s_time)
-    e_date, e_timestamp = parse_date(e_date, e_time)
+    s_date_obj, s_date = parse_date(s_date, s_time)
+    e_date_obj, e_date = parse_date(e_date, e_time)
 
     # sleep
     time.sleep(5)
@@ -70,25 +72,32 @@ def rentcar(s_date, e_date, s_time, e_time):
     time.sleep(0.5)
 
     # 차량 조회하고 예약하기 클릭
-    search = driver.find_element(By.XPATH, r'//*[text()="차량 조회하고 예약하기"]')
+    try:
+        search = driver.find_element(By.XPATH, r'//*[text()="차량 조회하고 예약하기"]')
+    except NoSuchElementException:
+        search = driver.find_element(By.XPATH, r'//*[text()="재검색"]')
+
     search.click()
     time.sleep(3)
 
     # html parse
     html = driver.page_source
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
 
     # SK렌트카에서 구분지은 블록들
     dill_box_wraps = soup.find_all(class_='dill_box_wrap')
 
-    # 각 블록마다
     for wrap in dill_box_wraps:
-        # 브랜드
-        print(wrap.find(class_='car_enter').text)
-        # 각 블록 안에 렌트 가능한 차량
-        for item in wrap.find_all(class_='list_item'):
-            print(item)
-            print()
+        rentcar_sql(wrap, s_date_obj, e_date_obj, s_time, e_time)
+
+    # # 각 블록마다
+    # for wrap in dill_box_wraps:
+    #     # 브랜드
+    #     print(wrap.find(class_='car_enter').text)
+    #     # 각 블록 안에 렌트 가능한 차량
+    #     for item in wrap.find_all(class_='list_item'):
+    #         print(item)
+    #         print()
 
 def get_date_combinations(start_date, days):
     'start_date부터 days일 후까지 가능한 시작/종료일 조합'
@@ -100,11 +109,73 @@ def get_date_combinations(start_date, days):
     combinations_list = list(combinations(date_list, 2))
     return combinations_list
 
+def rentcar_sql(wrap, s_date_obj: datetime, e_date_obj: datetime, s_time, e_time):
+    'sqlite3에 저장'
+    conn = sqlite3.connect('cars.db')
+    cursor = conn.cursor()
+
+    # column 이름
+    columns = ['brand', 'model', 'year_seating_capacity_etc', 'regular_price', 'sale_price', 'disccount_rate', 'start_timestamp', 'end_timestamp', 'start_date', 'end_date', 'start_time', 'end_time']
+
+    # ### DANGER ZONE
+    # time.sleep(30)
+    # # delete sql
+    # sql_delete = 'DROP TABLE IF EXISTS cars'
+    # cursor.execute(sql_delete)
+    # ###
+
+    # create sql
+    col_iter = iter(columns)
+    sql_create = f'''CREATE TABLE IF NOT EXISTS cars (
+        id SERIAL PRIMARY KEY,
+        {next(col_iter)} VARCHAR(10),
+        {next(col_iter)} VARCHAR(30),
+        {next(col_iter)} VARCHAR(50),
+        {next(col_iter)} INTEGER,
+        {next(col_iter)} INTEGER,
+        {next(col_iter)} INTEGER,
+        {next(col_iter)} TIMESTAMP,
+        {next(col_iter)} TIMESTAMP,
+        {next(col_iter)} DATE,
+        {next(col_iter)} DATE,
+        {next(col_iter)} TIME,
+        {next(col_iter)} TIME
+    )'''
+    cursor.execute(sql_create)
+
+    # send sql
+    send_data = []
+    brand = wrap.find(class_='car_enter').text.strip()
+    s_timestamp = s_date_obj.timestamp()
+    e_timestamp = e_date_obj.timestamp()
+    s_date = s_date_obj.strftime('%Y-%m-%D')
+    e_date = e_date_obj.strftime('%Y-%m-%D')
+    s_time = s_time + ':00'
+    e_time = e_time + ':00'
+    times = [s_timestamp, e_timestamp, s_date, e_date, s_time, e_time]
+
+    for item in wrap.find_all(class_='list_item'):
+        model = item.find(class_='main_name').text.strip()
+        year_and_capacity = item.find(class_='sb_text').text.strip()
+        regular_price = int(item.find(class_='price_regular').text.strip().replace('원', '').replace(',', '').replace('\u200c',''))
+        sale_price = int(item.find(class_='price_sale').text.strip().replace('원', '').replace(',', '').replace('\u200c',''))
+        discount_rate = int(item.find(class_='rate').text.strip('%').replace('\u200c',''))
+        send_data.append([brand, model, year_and_capacity, regular_price, sale_price, discount_rate]+times)
+        print(send_data)
+
+    sql_send = f'''INSERT INTO cars ({', '.join(columns).replace("'", '')})
+    VALUES {str(('?',)*len(columns)).replace("'", '')}
+    '''
+
+    cursor.executemany(sql_send, send_data)
+    conn.commit()
+    conn.close()
+
 # s_time == 07:30-22:00, e_time == 06:00-21:00
 s_time_h_range = list(range(8,22))
-s_times = [f'{i}:00' for i in s_time_h_range] + [f'{i}:30' for i in s_time_h_range] + ['7:30', '22:00']
+s_times = [f'{str(i).zfill(2)}:00' for i in s_time_h_range] + [f'{str(i).zfill(2)}:30' for i in s_time_h_range] + ['7:30', '22:00']
 e_time_h_range = list(range(6,21))
-e_times = [f'{i}:00' for i in e_time_h_range] + [f'{i}:30' for i in e_time_h_range] + ['21:00']
+e_times = [f'{str(i).zfill(2)}:00' for i in e_time_h_range] + [f'{str(i).zfill(2)}:30' for i in e_time_h_range] + ['21:00']
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
@@ -115,11 +186,11 @@ if __name__ == '__main__':
     DAYS= 60
     date_combos = get_date_combinations(s_date, DAYS)
 
-    rentcar(date_combos[5][0], date_combos[5][1], s_times[2], e_times[6])
+    # rentcar_crawl(date_combos[5][0], date_combos[5][1], s_times[2], e_times[6])
 
-    # for combo in date_combos:
-    #     for s_time in s_times:
-    #         for e_time in e_times:
-    #             rentcar(combo[0], combo[1], s_time, e_time)
+    for combo in date_combos:
+        for s_time in s_times:
+            for e_time in e_times:
+                rentcar_crawl(combo[0], combo[1], s_time, e_time)
 
     driver.quit()
