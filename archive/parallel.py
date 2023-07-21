98d222d5379f94308a33a8568b0ad0acc43fe4dd
jinmo <yjmd2222@gmail.com>
2023-07-21 14:52:55 +0900

    병렬 기각. 오류 원인 분석 필요. 현재로서는 포기..

---
+++ archive/parallel.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.common.exceptions import SessionNotCreatedException
+from selenium.webdriver.chrome.options import Options
+from concurrent import futures
+from datetime import datetime, timedelta
+
+from crawling_rentcar import rentcar_crawl, get_date_combinations
+
+s_date = datetime.today() + timedelta(days=1)
+date_combos = get_date_combinations(s_date)
+s_time_h_range = list(range(7,22+1))
+s_times = [f'{str(i).zfill(2)}:00' for i in s_time_h_range] + [f'{str(i).zfill(2)}:30' for i in s_time_h_range] + ['23:00']
+e_time_h_range = list(range(7,21+1))
+e_times = [f'{str(i).zfill(2)}:00' for i in e_time_h_range] + [f'{str(i).zfill(2)}:30' for i in e_time_h_range] + ['06:30']
+time_combos = [(i, j) for i in s_times for j in e_times]
+full_combos = [i+j for i in date_combos for j in time_combos]
+
+def selenium_title(combo):
+    'selenium 하나 실행'
+    # options = Options()
+    # options.add_argument('--headless')
+    try:
+        driver = webdriver.Chrome()
+    except SessionNotCreatedException:
+        return None
+    driver.maximize_window()
+    try:
+        rentcar_crawl(driver, *combo)
+    except Exception as error:
+        print(f'에러: {type(error).__name__}, 메시지: {error}')
+    title = driver.title
+    driver.quit()
+    return title
+
+with futures.ThreadPoolExecutor() as executor: # default/optimized number of threads
+    titles = list(executor.map(selenium_title, full_combos))
