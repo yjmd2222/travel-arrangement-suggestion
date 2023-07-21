1c7395e450aacdd8ffe1023c7b24ab33e017a1a8
jinmo <yjmd2222@gmail.com>
2023-07-21 18:01:03 +0900

    ?

--- scripts/crawling/crawling_rentcar.py
+++ scripts/crawling/crawling_rentcar.py
@@ -3,8 +3,7 @@ import csv
 from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.ui import Select
 
 from bs4 import BeautifulSoup
 
