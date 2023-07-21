e45ad59c6976ec18cb0a9223324a000565dda40c
jinmo <yjmd2222@gmail.com>
2023-07-21 15:50:39 +0900

    렌트카 데이터 전처리

---
+++ scripts/preprocessing/preprocessing-rentcar.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# 불러오기
+file_dir = 'data/raw/raw-cars.csv'
+df_cars = pd.read_csv(file_dir)
+df_cars.head()
+
+# 숫자만 표기
+# seats 인승 삭제
+df_cars['seats'] = df_cars['seats'].str[:-2].astype(int)
+# age_req 중간에 나이만 표기
+df_cars['age_req'] = df_cars['age_req'].str[1:-2].astype(int)
+# driving_experience 숫자만 표기
+df_cars['driving_experice'] = df_cars['driving_experice'].str[:-3]
+
+# 시간 type datetime으로 변경
+df_cars['start_date'] = pd.to_datetime(df_cars['start_date'])
+df_cars['end_date'] = pd.to_datetime(df_cars['end_date'])
+
+# 저장
+new_file_path = 'data/preprocessed/preprocessed-cars.csv'
+df_cars.to_csv(new_file_path, index=False)
